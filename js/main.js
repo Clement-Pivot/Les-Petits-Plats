428b77c8492939bcf9d96c157adf26748ac02cfe
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-10-23 17:09:15 +0200

    feat: populate ustensils list with items

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import { createCardDOM } from './utils/recipeCard.js'
 const recipesDOMContainer = document.querySelector('.recipe-container')
 const ingredientList = new Set()
 const applianceList = new Set()
+const ustensilsList = new Set()
 recipes.forEach(item => {
   item.DOM = createCardDOM(item)
   recipesDOMContainer.appendChild(item.DOM)
@@ -15,4 +16,9 @@ recipes.forEach(item => {
   if ([...applianceList].indexOf(item.appliance) < 0) {
     applianceList.add(item.appliance)
   }
+  item.ustensils.forEach(ust => {
+    if ([...ustensilsList].indexOf(ust) < 0) {
+      ustensilsList.add(ust)
+    }
+  })
 })
