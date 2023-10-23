650107f9d04c4116b9e744f6b646a87a1a4b49aa
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-10-23 17:08:26 +0200

    feat: populate ingredient list with items

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ import { recipes } from '../data/recipes.js'
 import { createCardDOM } from './utils/recipeCard.js'
 
 const recipesDOMContainer = document.querySelector('.recipe-container')
+const ingredientList = new Set()
 recipes.forEach(item => {
   item.DOM = createCardDOM(item)
   recipesDOMContainer.appendChild(item.DOM)
+  item.ingredients.forEach(ingr => {
+    if ([...ingredientList].indexOf(ingr.ingredient) < 0) {
+      ingredientList.add(ingr.ingredient)
+    }
+  })
 })
