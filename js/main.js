6c5f379968ed77e518bfc5a22323f5c049631be6
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-10-23 16:46:30 +0200

    feat: add recipes from file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 import { recipes } from '../data/recipes.js'
 import { createCardDOM } from './utils/recipeCard.js'
+
+const recipesDOMContainer = document.querySelector('.recipe-container')
+recipes.forEach(item => {
+  item.DOM = createCardDOM(item)
+  recipesDOMContainer.appendChild(item.DOM)
+})
